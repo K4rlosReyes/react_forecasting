5d614e9233290a23dc3f32eb2730c32e9e2f61a6
K4rlosReyes <carlosreyesml18@gmail.com>
2022-12-19 17:41:41 -0500

    Fixed function names

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def index():
     return send_file('web/index.html')
 
 @app.route('/forecast')
-def get_results():
+def get_forecast():
     return requests.get(url = f"{BASE_URL}/forecast").json()
 
 @app.route('/real')
-def get_input():
+def get_real():
     return requests.get(url = f"{BASE_URL}/real").json()
 
 @app.route('/prevforecast')
-def get_real():
+def get_prevforecast():
     return requests.get(url = f"{BASE_URL}/prevforecast").json()
